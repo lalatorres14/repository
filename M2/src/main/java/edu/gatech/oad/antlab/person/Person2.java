f0245e308efd40bbed4add257492504594c06996
jennakohls <jennakohls@gmail.com>
2019-01-25 15:04:49 -0500

    fixed syntax errors

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,10 +1,12 @@
 package edu.gatech.oad.antlab.person;
 import  java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Paula
  * @version 1.1
@@ -47,7 +49,7 @@ public class Person2 {
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
